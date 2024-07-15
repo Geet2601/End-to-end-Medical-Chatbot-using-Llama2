df7907fd5e32700c0c275158f8a26398c6f5cff4
Geet2601 <gt2601001gt@gmail.com>
2024-07-15 23:55:08 +0530

    updated code for 2024

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Medical Chatbot',
+    version= '0.0.0',
+    author= 'Geet Tiwari',
+    author_email= 'gt260101001gt@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
